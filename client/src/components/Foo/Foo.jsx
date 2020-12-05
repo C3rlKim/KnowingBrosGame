a4d21fbcc7b2be4e92dc4603efb53bc85ab4fcd3
Karl Kim <karlkim@Karls-MBP.lan>
2020-12-05 07:18:57 -0800

    boiler plate for client side socket.io

--- client/src/components/Foo/Foo.jsx
+++ client/src/components/Foo/Foo.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // Import socket.io client
 import io from "socket.io-client";
@@ -6,12 +6,14 @@ import io from "socket.io-client";
 // ENDPOINT of socket.io server
 const ENDPOINT = 'http://localhost:5000'
 
-let socket;
-
 const  Foo = () => {
-  // socket.io connection started
-  socket = io(ENDPOINT);
-  
+  // Empty array as 2nd argument:
+  // Rendered only onmount and unmount
+  useEffect(() => {
+    // socket.io connection started
+    const socket = io(ENDPOINT);
+  },[]);
+
   return <h1>Karl and Kelley's Game Client</h1>
 }
 
