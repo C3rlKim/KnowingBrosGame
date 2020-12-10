bdc34dc669325033c72b196456f4ce2d496d0845
Karl Kim <kim648@usc.edu>
2020-12-10 05:41:17 -0800

    merged chat.jsx

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,39 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 import io from 'socket.io-client';
 
 const ENDPOINT = "http://localhost:5000";
-
-// How do we get name and room in to this component
-let socket;
+// Should the connection begin in login?
+let socket = io(ENDPOINT);
 
 const Chat = () => {
-  const [id, setId] = useState();
-  const [message, setMessage] = useState("");
+  const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
 
+  const location = useLocation();
+  const roomName = useRef(location.state.roomName);
+  const userName = useRef(location.state.userName);
+
   useEffect(() => {
-    // Start socket connection
-    socket = io(ENDPOINT);
-    socket.on("id",(socketId) => {
-      setId(socketId);
-    })
-    socket.on("message", ({ message, id }) => {
-      setMessages(prevMsgs =>[ ...prevMsgs, message ]);
+    // Request to join room
+    socket.emit("join", roomName.current);
+
+    // Listening to message from server
+    socket.on("serverMessage", (msgObj) => {
+      setMessages(prevMsgs =>[ ...prevMsgs, msgObj ]);
     });
   }, []);
 
   const sendMessage = event => {
     event.preventDefault();
 
-    socket.emit("sendMessage",{ message, id });
-    setMessage("");
+    socket.emit("sendMessage",{ input, userName: userName.current, roomName: roomName.current }, () => {
+      // socket.io acknowledgement
+      setInput("");
+    });
   }
 
   return (
     <div>
-      <p>Your id is: {id} </p>
-      {messages.map((message, idx) => <p key={idx}>{message}</p>)}
-      <input type="text" value={message} onChange={({ target : { value }}) => setMessage(value)}/>
+      <p>Room name: {roomName.current}</p>
+      {messages.map((msgObj, idx) => <p key={idx}>{msgObj.userName} : {msgObj.message}</p>)}
+      <input type="text"
+        value={input}
+        onChange={({ target : { value }}) => setInput(value)}
+        onKeyPress={ (event) => event.key === "Enter" ? sendMessage(event) : null}
+      />
       <input type="submit" onClick={sendMessage}/>
     </div>
   );
