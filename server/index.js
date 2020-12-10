276c05cf203607dbed0c866e2f7a2e4137ff2391
Karl Kim <kim648@usc.edu>
2020-12-10 05:40:04 -0800

    merged server index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
-// Imports for express
 const express = require('express');
 const app = express();
-// Imports for socketio
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
@@ -14,10 +12,14 @@ const PORT = process.env.PORT || 5000;
 
 // Start of connection
 io.on("connect", socket => {
-  socket.emit('id', socket.id);
-
-  socket.on("sendMessage",({ message, id }) => {
-    io.emit("message", { message, id });
+  // Join socket to a room specified by client
+  socket.on("join", (roomName) => {
+    socket.join(roomName);
+  })
+  // listen client's sendMessage and emits message to the room
+  socket.on("sendMessage",({ input: message, userName, roomName }, msgConfirm) => {
+    io.in(roomName).emit("serverMessage", { message, userName });
+    msgConfirm();
   });
 });
 
