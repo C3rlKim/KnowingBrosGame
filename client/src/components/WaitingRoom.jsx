78a6a9e70f092ccc0d3ea3e4e825963e2d147f0a
Kelley Nguyen <kelleyng@usc.edu>
2020-12-10 13:55:07 -0800

    style waiting room

--- client/src/components/WaitingRoom.jsx
+++ client/src/components/WaitingRoom.jsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import AccentButton from './AccentButton';
+import '../style/CreateJoinWait.scss';
 
 const hardcodedList = ["Kelley", "Karl"];
 
 const players = hardcodedList.map((name) => {
   return (
-    <li>{name}</li>
+    <div>{name}</div>
   );
 })
 
 const WaitingRoom = () => {
+  const history = useHistory();
+  const [spin, setSpin] = useState(false);
+
+  useEffect( () => {
+    if (spin) {
+      let timer = setTimeout(() => setSpin(false), 2000);
+      return () => {
+        clearTimeout(timer)
+      }
+    }
+
+  }, [spin])
+
+  const handleCloudClick = (e) => {
+    setSpin(true);
+  }
+
+  const handleLeave =() => {
+    //notify other players
+    //return to landing page
+    history.push('/');
+  }
+
   return (
-    <div>
-      <h1>waiting room</h1>
-      <p>host will start the game one all players have joined</p>
-      <p>no one will be able to join once it starts</p>
-      <Button>leave game</Button>
-      <h6>PLAYERS</h6>
-      <ul>
-        {players}
-      </ul>
-    </div>
+    <Container fluid className="purple">
+      <div id="background-wrap">
+        <div className="x1">
+          <div className="cloud"></div>
+        </div>
+
+        <div className="x2">
+          <div className="cloud"></div>
+        </div>
+
+        <div className="x3">
+          <div className="cloud"></div>
+        </div>
+
+        <div className="x4">
+          <div id="special" className={spin ? "cloud spin" : "cloud"} onClick={handleCloudClick}></div>
+        </div>
+
+        <div className="x5">
+          <div className="cloud"></div>
+        </div>
+      </div>
+
+      <Row className="justify-content-center">
+        <Col xs={12} md={6} lg={3} className="m-auto">
+          <h1>waiting room</h1>
+          <p className="white">host will start the game one all players have joined</p>
+          <p className="white">no one will be able to join once it starts</p>
+          <AccentButton onClick={handleLeave}>leave game</AccentButton>
+          <h6>PLAYERS</h6>
+          <div className="white">
+            {players}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
